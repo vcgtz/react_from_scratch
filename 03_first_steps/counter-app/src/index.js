8decc346d9593e78c9749ee3b0fcb65468028484
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-08 12:49:51 -0500

    03 - Hola Mundo en react

--- 03_first_steps/counter-app/src/index.js
+++ 03_first_steps/counter-app/src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const greeting = <h1>Hello World!</h1>; // H1 Con un saludo usando JSX
+const divRoot = document.querySelector('#root'); // Referencia el div principal de la aplicación
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(greeting, divRoot); // Escribimos nuestro saludo en el div principal de la aplicación
