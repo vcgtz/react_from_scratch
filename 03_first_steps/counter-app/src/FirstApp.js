2fbb2608487ad60083945bdc46ffd266be2722eb
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-09 08:56:03 -0500

    03 - Usar defaultProps para definir valores por defecto en las propiedades

--- 03_first_steps/counter-app/src/FirstApp.js
+++ 03_first_steps/counter-app/src/FirstApp.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FirstApp = ({ otherMessage = 'No message' }) => { // Recibir propiedades
+const FirstApp = ({ otherMessage = 'No message', subtitle }) => { // Recibir propiedades
   const message = 'This is message in a variable';
 
   return ( // Solamente puede regresar un elemento, por eso se usa un high order component para agrupar toda la información
     <>
       <h1>Hello World!!!</h1>
+      <h2>{ subtitle }</h2>
       <p>{ message }</p>
       <p>{ otherMessage }</p>
     </>
@@ -16,6 +17,10 @@ const FirstApp = ({ otherMessage = 'No message' }) => { // Recibir propiedades
 
 FirstApp.propTypes = {
   otherMessage: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+  subtitle: 'The Subtitle'
 }
 
 export default FirstApp; // Exportamos nuestro componente
