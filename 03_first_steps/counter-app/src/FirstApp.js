ded75ef550e65b3c3686a75dc74c816a350f1a4b
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-09 08:35:12 -0500

    03 - Imprimir variables en el HTML

--- 03_first_steps/counter-app/src/FirstApp.js
+++ 03_first_steps/counter-app/src/FirstApp.js
@@ -1,23 +1,25 @@
 // Functional Components
-import React, { Fragment } from 'react'; // Se debe importar React donde se use JSX
-// import React from 'react'; // Se debe importar React donde se use JSX
+import React from 'react'; // Se debe importar React donde se use JSX
 
 const FirstApp = () => { // Regresar un fragment component
-  return ( // Solamente puede regresar un elemento, por eso se usa un high order component para agrupar toda la información
-    <Fragment>
-      <h1>Hello World!!!</h1>
-      <p>This is a message</p>
-    </Fragment>
-  );
+  const message = 'This is message in a variable';
 
-  /*
+  // <> </> es equivalente <Fragment></Fragment>
   return ( // Solamente puede regresar un elemento, por eso se usa un high order component para agrupar toda la información
-    <> // Forma corta de usar un fragment
+    <>
       <h1>Hello World!!!</h1>
       <p>This is a message</p>
+      <p>{ message }</p>
+      <p>{ 3 * 5 / 2 * 87 }</p>
+      <p>{ Math.random() }</p>
+      <p>{ [1, 2, 3, 4, 5] }</p>
+      <p>{ JSON.stringify({a: 1, b: 2}) }</p>
+      <pre>{ JSON.stringify({a: 1, b: 2}, null, 2) }</pre>
+      {/* Esto es un comentario */}
     </>
   );
-  */
+
+  // Para imprimir variables usamos {}, dentro de las {} se puede usar cualquier expresión de JS, menos objetos
 };
 
 export default FirstApp; // Exportamos nuestro componente
