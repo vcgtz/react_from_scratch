1b3768ee078675cc4ed22a33716fc5a10e360014
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-09 08:28:01 -0500

    03 - Retornar elementos en un fragment

--- 03_first_steps/counter-app/src/FirstApp.js
+++ 03_first_steps/counter-app/src/FirstApp.js
@@ -1,8 +1,23 @@
 // Functional Components
-import React from 'react'; // Se debe importar React donde se use JSX
+import React, { Fragment } from 'react'; // Se debe importar React donde se use JSX
+// import React from 'react'; // Se debe importar React donde se use JSX
 
-const FirstApp = () => { // Componente basado en una función que regresa un H1 con un mensaje
-  return <h1>Hello World!!!</h1>;
+const FirstApp = () => { // Regresar un fragment component
+  return ( // Solamente puede regresar un elemento, por eso se usa un high order component para agrupar toda la información
+    <Fragment>
+      <h1>Hello World!!!</h1>
+      <p>This is a message</p>
+    </Fragment>
+  );
+
+  /*
+  return ( // Solamente puede regresar un elemento, por eso se usa un high order component para agrupar toda la información
+    <> // Forma corta de usar un fragment
+      <h1>Hello World!!!</h1>
+      <p>This is a message</p>
+    </>
+  );
+  */
 };
 
 export default FirstApp; // Exportamos nuestro componente
