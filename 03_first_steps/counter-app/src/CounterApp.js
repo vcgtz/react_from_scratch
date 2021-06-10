50a29d9a38db6b00027a72dda15914418730dd68
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-10 08:58:55 -0500

    03 - Manejar el evento onClick

--- 03_first_steps/counter-app/src/CounterApp.js
+++ 03_first_steps/counter-app/src/CounterApp.js
@@ -2,10 +2,16 @@ import react from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value = 0 }) => {
+  // handleAdd
+  const handleAdd = (e) => {
+    console.log(e);
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{ value }</h2>
+      <button onClick={ handleAdd }>+1</button>
     </>
   )
 };
