5d101c87ddb3d8b88ecf2a5908e4ad698b732369
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-10 09:12:28 -0500

    03 - Agregar metodos para aumentar, restar y resetear el state

--- 03_first_steps/counter-app/src/CounterApp.js
+++ 03_first_steps/counter-app/src/CounterApp.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value = 0 }) => {
-  const [counter, setCounter] = useState(0); // retorna un arreglo value, function
+  const [counter, setCounter] = useState(value); // retorna un arreglo value, function
 
   // handleAdd
   const handleAdd = (e) => {
@@ -10,11 +10,21 @@ const CounterApp = ({ value = 0 }) => {
     // setCounter((c) => c + 1); La funcion recibe el valor previo del counter
   }
 
+  const handleSubtract = (e) => {
+    setCounter(counter - 1);
+  };
+
+  const handleReset = (e) => {
+    setCounter(value);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{ counter }</h2>
       <button onClick={ handleAdd }>+1</button>
+      <button onClick={ handleSubtract }>-1</button>
+      <button onClick={ handleReset }>Reset</button>
     </>
   )
 };
