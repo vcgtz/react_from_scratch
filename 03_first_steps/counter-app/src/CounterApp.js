57754b37d5f22c4c769742d60eae399723e0c05f
Vicente Gutiérrez <gtz.rvera@gmail.com>
2021-06-10 09:09:55 -0500

    03 - Agregar el hook useState

--- 03_first_steps/counter-app/src/CounterApp.js
+++ 03_first_steps/counter-app/src/CounterApp.js
@@ -1,16 +1,19 @@
-import react from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value = 0 }) => {
+  const [counter, setCounter] = useState(0); // retorna un arreglo value, function
+
   // handleAdd
   const handleAdd = (e) => {
-    console.log(e);
+    setCounter(counter + 1);
+    // setCounter((c) => c + 1); La funcion recibe el valor previo del counter
   }
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{ value }</h2>
+      <h2>{ counter }</h2>
       <button onClick={ handleAdd }>+1</button>
     </>
   )
